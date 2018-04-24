f81cc5001c785478b10bb78be0fdce55c8e74137
Kenneth <kendomine@yahoo.com>
2018-04-24 10:52:29 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+
+
+mongoose.connect("mongodb://kendomine:"+
+ process.env.MONGO_ATLAS_PW+
+  "@ohmygreen-rest-shard-00-00-iqlji.mongodb.net:27017,ohmygreen-rest-shard-00-01-iqlji.mongodb.net:27017,ohmygreen-rest-shard-00-02-iqlji.mongodb.net:27017/test?ssl=true&replicaSet=ohmygreen-rest-shard-0&authSource=admin"
+);
+mongoose.Promise = global.Promise;
+//Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routes which handle requests
+app.use('/products', productRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Not Found');
+	error.status = 404;
+	next(error);
+})
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
+module.exports = app;
